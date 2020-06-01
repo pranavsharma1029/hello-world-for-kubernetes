174b617871fa04bf04d37ca7dabee8f0f43e1d1e
Pranav Sharma <pranavsharma1029@gmail.com>
2020-06-01 18:18:53 +0530

    display messgae

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello world for kubernetes
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           Hello world for kubernetes
-        </a>
+       
       </header>
     </div>
   );
